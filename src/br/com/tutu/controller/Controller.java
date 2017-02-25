5d6b491e6bcc6fc82344844bae531ddde859eeaf
Raimundo Campos <raimundojr89@gmail.com>
2017-02-25 19:48:48 -0300

    Commit inicial

---
+++ src/br/com/tutu/controller/Controller.java
@@ -0,0 +1,19 @@
+package br.com.tutu.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/login")
+public class Controller extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/paginas/login.html");
+		dispatcher.forward(req, resp);
+	}
+}
